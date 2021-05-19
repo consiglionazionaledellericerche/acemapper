86d09dc4102f8e6a36c2af54c0ea5b618dce9524
PIERLEONI FABRIZIO <fabrizio.pierleoni@cnr.it>
2021-05-19 17:52:51 +0200

    fix cf

--- src/main/java/it/cnr/si/AceOIDCProtocolMapper.java
+++ src/main/java/it/cnr/si/AceOIDCProtocolMapper.java
@@ -2,6 +2,7 @@ package it.cnr.si;
 
 import it.cnr.si.service.AceService;
 import it.cnr.si.service.dto.anagrafica.simpleweb.SimpleRuoloWebDto;
+import it.cnr.si.service.dto.anagrafica.simpleweb.UtenteWebDto;
 import org.keycloak.models.ClientSessionContext;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.ProtocolMapperModel;
@@ -13,21 +14,26 @@ import org.keycloak.representations.AccessToken;
 
 import java.util.*;
 import org.jboss.logging.Logger;
+import org.keycloak.representations.IDToken;
+
 import java.util.stream.Collectors;
 
 public class AceOIDCProtocolMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper {
+
+    private static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
+
+    static {
+        OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, FullNameMapper.class);
+    }
 
     public static final String PROVIDER_ID = "oidc-customprotocolmapper";
     public static final String DISPLAY_NAME = "ace mapper";
     public static final String HELP_TEXT = "role and context mapper";
 
-    private static final List<ProviderConfigProperty> configProperties = new ArrayList();
-
     private static final Logger LOGGER = Logger.getLogger(AceOIDCProtocolMapper.class);
 
     private AceService aceService = new AceService();
 
-    @Override
     public List<ProviderConfigProperty> getConfigProperties() {
         return configProperties;
     }
@@ -52,14 +58,30 @@ public class AceOIDCProtocolMapper extends AbstractOIDCProtocolMapper implements
         return HELP_TEXT;
     }
 
-    public AccessToken transformAccessToken(AccessToken token, ProtocolMapperModel mappingModel, KeycloakSession keycloakSession,
-                                            UserSessionModel userSession, ClientSessionContext clientSessionCtx) {
+    protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession,
+                            KeycloakSession keycloakSession, ClientSessionContext clientSessionCtx) {
 
         Map<String, Map<String, Set<String>>> contexts = new HashMap<>();
 
+        // ldap o spid username
+        String username = userSession.getUser().getUsername();
         try {
-            String username = userSession.getUser().getUsername();
+
+            // nel caso di username spid
+            if(username.toUpperCase().startsWith("TINIT")) {
+
+                try {
+                    String codiceFiscale = username.substring(6).toUpperCase();
+                    String ldapUsername = aceService.getUtenteByCodiceFiscale(codiceFiscale).getUsername().toLowerCase();
+                    //userSession.getUser().setUsername(ldapUsername);
+                    username = ldapUsername;
+                } catch (Exception e) {
+                    LOGGER.info("utente " + username + " spid non presente in ldap");
+                }
+            }
+
             LOGGER.info(username);
+
             List<SimpleRuoloWebDto> simpleRuoloWebDtos = aceService.ruoliAttivi(username);
 
             List<String> contesti = simpleRuoloWebDtos.stream()
@@ -81,9 +103,9 @@ public class AceOIDCProtocolMapper extends AbstractOIDCProtocolMapper implements
             LOGGER.error(e);
         }
         token.getOtherClaims().put("contexts", contexts);
+        token.getOtherClaims().put("preferred_username", username);
+        token.getOtherClaims().put("username_cnr", username);
 
-        setClaim(token, mappingModel, userSession, keycloakSession, clientSessionCtx);
-        return token;
     }
 
     public static ProtocolMapperModel create(String name, boolean accessToken, boolean idToken, boolean userInfo) {
@@ -91,9 +113,10 @@ public class AceOIDCProtocolMapper extends AbstractOIDCProtocolMapper implements
         mapper.setName(name);
         mapper.setProtocolMapper(PROVIDER_ID);
         mapper.setProtocol(OIDCLoginProtocol.LOGIN_PROTOCOL);
-        Map<String, String> config = new HashMap();
-        config.put(OIDCAttributeMapperHelper.INCLUDE_IN_ACCESS_TOKEN, "true");
-        config.put(OIDCAttributeMapperHelper.INCLUDE_IN_ID_TOKEN, "true");
+        Map<String, String> config = new HashMap<>();
+        if (accessToken) config.put(OIDCAttributeMapperHelper.INCLUDE_IN_ACCESS_TOKEN, "true");
+        if (idToken) config.put(OIDCAttributeMapperHelper.INCLUDE_IN_ID_TOKEN, "true");
+        if (userInfo) config.put(OIDCAttributeMapperHelper.INCLUDE_IN_USERINFO, "true");
         mapper.setConfig(config);
         return mapper;
     }
